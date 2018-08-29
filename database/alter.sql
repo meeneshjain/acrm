013de97b59fc5f57f1b058930dc4fcf3b090e542
manish carpenter <manishcrpntr2@gmail.com>
2018-08-29 23:55:58 +0530

    Alter Sql

---
+++ database/alter.sql
@@ -0,0 +1,30 @@
+MANISH 29-08-2018
+
+-- Adminer 4.2.5 MySQL dump
+
+SET NAMES utf8;
+SET time_zone = '+00:00';
+
+DROP TABLE IF EXISTS `companies`;
+CREATE TABLE `companies` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `company_name` varchar(250) NOT NULL,
+  `subscription` int(11) NOT NULL,
+  `email_1` varchar(100) NOT NULL,
+  `email_2` varchar(100) NOT NULL,
+  `contact_1` varchar(50) NOT NULL,
+  `contact_2` varchar(50) NOT NULL,
+  `pan_no` varchar(50) NOT NULL,
+  `gstin_no` varchar(50) NOT NULL,
+  `address` varchar(50) NOT NULL,
+  `about_company` text NOT NULL,
+  `created_date` datetime NOT NULL,
+  `updated_date` datetime NOT NULL,
+  `status` tinyint(4) NOT NULL DEFAULT '0',
+  `is_deleted` tinyint(4) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `subscription` (`subscription`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+-- 2018-08-29 18:18:07
